c93fd611d2fc98386237f0062ca878a8149d540b
Daniel Slapman <danslapman@gmail.com>
2024-02-10 23:13:40 +0100

    Fix readStr

--- backend/utils/src/main/scala/ru/tinkoff/tcb/utils/resource/package.scala
+++ backend/utils/src/main/scala/ru/tinkoff/tcb/utils/resource/package.scala
@@ -4,13 +4,5 @@ import scala.io.Source
 import scala.util.Using
 
 package object resource {
-  def getResPath(fileName: String): String = getClass.getResource(s"/$fileName").getPath
-
-  def readBytes(fileName: String): Array[Byte] = {
-    val path = getResPath(fileName)
-    import java.nio.file.{Files, Paths}
-    Files.readAllBytes(Paths.get(path))
-  }
-
-  def readStr(fileName: String): String = Using.resource(Source.fromFile(getResPath(fileName)))(_.mkString)
+  def readStr(fileName: String): String = Using.resource(Source.fromResource(fileName))(_.mkString)
 }
