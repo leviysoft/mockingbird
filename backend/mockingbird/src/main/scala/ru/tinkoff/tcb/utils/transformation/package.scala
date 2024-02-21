a5c37154143d01ac4736d7b75194184874ba5dd8
Daniel Slapman <danslapman@gmail.com>
2024-02-21 22:31:31 +0100

    Remove unused regexes

--- backend/mockingbird/src/main/scala/ru/tinkoff/tcb/utils/transformation/package.scala
+++ backend/mockingbird/src/main/scala/ru/tinkoff/tcb/utils/transformation/package.scala
@@ -6,16 +6,4 @@ package object transformation {
   val SubstRx: Regex = """\$\{(.*?)\}""".r
   val FunRx: Regex   = """%\{.*?\}""".r
   val CodeRx: Regex  = """%\{(.*?)\}""".r
-
-  val RandStr: Regex          = """%\{randomString\((\d+)\)\}""".r
-  val RandAlphabetStr: Regex  = """%\{randomString\(\"(.*?)\",\s*(\d+),\s*(\d+)\)\}""".r
-  val RandNumStr: Regex       = """%\{randomNumericString\((\d+)\)\}""".r
-  val RandInt: Regex          = """%\{randomInt\((\d+)\)\}""".r
-  val RandIntInterval: Regex  = """%\{randomInt\((\d+),\s*(\d+)\)\}""".r
-  val RandLong: Regex         = """%\{randomLong\((\d+)\)\}""".r
-  val RandLongInterval: Regex = """%\{randomLong\((\d+),\s*(\d+)\)\}""".r
-  val RandUUID: Regex         = """%\{UUID\}""".r
-
-  val Today: Regex = """%\{today\((.*?)\)\}""".r
-  val Now: Regex   = """%\{now\((.*?)\)\}""".r
 }
