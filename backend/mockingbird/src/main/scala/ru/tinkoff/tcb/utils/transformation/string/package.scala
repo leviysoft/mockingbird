ee4ab9fb9e6a12e5ac56b82d14eb47623629f0dc
Daniel Slapman <danslapman@gmail.com>
2024-03-10 22:57:53 +0100

    Support templates in response headers

--- backend/mockingbird/src/main/scala/ru/tinkoff/tcb/utils/transformation/string/package.scala
+++ backend/mockingbird/src/main/scala/ru/tinkoff/tcb/utils/transformation/string/package.scala
@@ -8,6 +8,9 @@ import ru.tinkoff.tcb.utils.transformation.json.*
 
 package object string {
   implicit final class StringTransformations(private val s: String) extends AnyVal {
+    def isTemplate: Boolean =
+      CodeRx.findFirstIn(s).isDefined || SubstRx.findFirstIn(s).isDefined
+
     def substitute(jvalues: Json, xvalues: Node)(implicit sandbox: GraalJsSandbox): String =
       if (SubstRx.findFirstIn(s).isDefined || CodeRx.findFirstIn(s).isDefined)
         Json.fromString(s).substitute(jvalues).substitute(xvalues).asString.getOrElse(s)
