e812efd6e03b66455ce0c1076a3fccaa5f1f5934
Daniel Slapman <danslapman@gmail.com>
2024-03-31 19:07:13 +0200

    Translate CatsFacts

--- backend/edsl/src/test/scala/ru/tinkoff/tcb/mockingbird/examples/CatsFacts.scala
+++ backend/edsl/src/test/scala/ru/tinkoff/tcb/mockingbird/examples/CatsFacts.scala
@@ -8,17 +8,17 @@ import ru.tinkoff.tcb.mockingbird.edsl.model.ValueMatcher.syntax.*
 
 class CatsFacts[HttpResponseR] extends ExampleSet[HttpResponseR] {
 
-  override val name = "Примеры использования ExampleSet"
+  override val name = "Examples of using ExampleSet"
 
-  example("Получение случайного факта о котиках")(
+  example("Getting a random fact about kittens")(
     for {
-      _ <- describe("Отправить GET запрос")
+      _ <- describe("Send a GET request")
       resp <- sendHttp(
         method = Get,
         path = "/fact",
         headers = Seq("X-CSRF-TOKEN" -> "unEENxJqSLS02rji2GjcKzNLc0C0ySlWih9hSxwn")
       )
-      _ <- describe("Ответ содержит случайный факт полученный с сервера")
+      _ <- describe("The response contains a randomly obtained fact from the server")
       _ <- checkHttp(
         resp,
         HttpResponseExpected(
