e5c520f2f64a09f8e884dc3e3fa0eed43efa4d06
Daniel Slapman <danslapman@gmail.com>
2024-03-31 23:49:51 +0200

    Lint edsl

--- backend/edsl/src/main/scala/ru/tinkoff/tcb/mockingbird/edsl/ExampleSet.scala
+++ backend/edsl/src/main/scala/ru/tinkoff/tcb/mockingbird/edsl/ExampleSet.scala
@@ -27,8 +27,8 @@ trait ExampleSet[HttpResponseR] {
     liftF[Step, Unit](Describe(text, pos))
 
   /**
-   * In tests, makes an HTTP request with the specified parameters or adds a sample request to the Markdown,
-   * which can be executed with the curl command.
+   * In tests, makes an HTTP request with the specified parameters or adds a sample request to the Markdown, which can
+   * be executed with the curl command.
    *
    * @param method
    *   HTTP method used.
@@ -41,9 +41,8 @@ trait ExampleSet[HttpResponseR] {
    * @param query
    *   URL query parameters.
    * @return
-   *   Returns an object representing the result of the request execution;
-   *   the specific type depends on the DSL interpreter.
-   *   The return value can only be used by passing it to the [[checkHttp]] method.
+   *   Returns an object representing the result of the request execution; the specific type depends on the DSL
+   *   interpreter. The return value can only be used by passing it to the [[checkHttp]] method.
    */
   final def sendHttp(
       method: HttpMethod,
@@ -57,20 +56,18 @@ trait ExampleSet[HttpResponseR] {
     liftF[Step, HttpResponseR](SendHttp[HttpResponseR](HttpRequest(method, path, body, headers, query), pos))
 
   /**
-   * In tests, verifies that the received HTTP response matches the expectations.
-   * When generating Markdown, inserts the expected response based on the specified expectations.
-   * If no expectations are specified, nothing will be added.
+   * In tests, verifies that the received HTTP response matches the expectations. When generating Markdown, inserts the
+   * expected response based on the specified expectations. If no expectations are specified, nothing will be added.
    *
    * @param response
    *   the result of executing [[sendHttp]], the type depends on the DSL interpreter.
    * @param expects
-   *   expectations placed on the result of the HTTP request.
-   *   Expectations concern the response code, request body, and headers received from the server.
+   *   expectations placed on the result of the HTTP request. Expectations concern the response code, request body, and
+   *   headers received from the server.
    * @return
-   *   returns the parsed response from the server. When generating Markdown,
-   *   since there is no actual response from the server, it constructs a response based
-   *   on the provided response expectations.
-   *   Only information relevant to the specified checks is added to the Markdown.
+   *   returns the parsed response from the server. When generating Markdown, since there is no actual response from the
+   *   server, it constructs a response based on the provided response expectations. Only information relevant to the
+   *   specified checks is added to the Markdown.
    */
   final def checkHttp(response: HttpResponseR, expects: HttpResponseExpected)(implicit
       pos: source.Position
