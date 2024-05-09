a487cb1e4be48ae7907b1fb4c38a49e4eaeeb89b
a.e.aminova <a.e.aminova@tinkoff.ru>
2024-05-09 16:55:03 +0300

    Purge ephemeral grpc stubs

--- backend/mockingbird/src/main/scala/ru/tinkoff/tcb/mockingbird/stream/EphemeralCleaner.scala
+++ backend/mockingbird/src/main/scala/ru/tinkoff/tcb/mockingbird/stream/EphemeralCleaner.scala
@@ -10,13 +10,22 @@ import zio.interop.catz.*
 
 import ru.tinkoff.tcb.criteria.*
 import ru.tinkoff.tcb.criteria.Typed.*
+import ru.tinkoff.tcb.mockingbird.dal.GrpcMethodDescriptionDAO
+import ru.tinkoff.tcb.mockingbird.dal.GrpcStubDAO
 import ru.tinkoff.tcb.mockingbird.dal.HttpStubDAO
 import ru.tinkoff.tcb.mockingbird.dal.ScenarioDAO
+import ru.tinkoff.tcb.mockingbird.model.GrpcMethodDescription
+import ru.tinkoff.tcb.mockingbird.model.GrpcStub
 import ru.tinkoff.tcb.mockingbird.model.HttpStub
 import ru.tinkoff.tcb.mockingbird.model.Scenario
 import ru.tinkoff.tcb.mockingbird.model.Scope
 
-final class EphemeralCleaner(stubDAO: HttpStubDAO[Task], scenarioDAO: ScenarioDAO[Task]) {
+final class EphemeralCleaner(
+    stubDAO: HttpStubDAO[Task],
+    scenarioDAO: ScenarioDAO[Task],
+    grpcStubDAO: GrpcStubDAO[Task],
+    grpcMethodDescriptionDAO: GrpcMethodDescriptionDAO[Task]
+) {
   private val log: Logging[UIO] = new ZUniversalLogging(this.getClass.getName)
 
   private val cronScheduler = Cron4sScheduler.systemDefault[Task]
@@ -37,18 +46,32 @@ final class EphemeralCleaner(stubDAO: HttpStubDAO[Task], scenarioDAO: ScenarioDA
         prop[Scenario](_.scope).in[Scope](Scope.Ephemeral, Scope.Countdown) && prop[Scenario](_.created) < threshold
       )
       _ <- log.info("Purging expired scenarios: {} deleted", deleted2)
-      deleted3 <- stubDAO.delete(
+      methodDescriptions <- grpcMethodDescriptionDAO.findChunk(
+        prop[GrpcMethodDescription](_.scope).in[Scope](Scope.Ephemeral, Scope.Countdown),
+        0,
+        Int.MaxValue
+      )
+      deleted3 <- grpcStubDAO.delete(
+        prop[GrpcStub](_.methodDescriptionId).in(methodDescriptions.map(_.id)) && prop[GrpcStub](_.created) < threshold
+      )
+      _ <- log.info("Purging expired grpc stubs: {} deleted", deleted3)
+      deleted4 <- stubDAO.delete(
         prop[HttpStub](_.scope) === Scope.Countdown.asInstanceOf[Scope] && prop[HttpStub](_.times) <= Option(
           refineMV[NonNegative](0)
         )
       )
-      _ <- log.info("Purging countdown stubs: {} deleted", deleted3)
-      deleted4 <- scenarioDAO.delete(
+      _ <- log.info("Purging countdown stubs: {} deleted", deleted4)
+      deleted5 <- scenarioDAO.delete(
         prop[Scenario](_.scope) === Scope.Countdown.asInstanceOf[Scope] && prop[Scenario](_.times) <= Option(
           refineMV[NonNegative](0)
         )
       )
-      _ <- log.info("Purging countdown scenarios: {} deleted", deleted4)
+      _ <- log.info("Purging countdown scenarios: {} deleted", deleted5)
+      deleted6 <- grpcStubDAO.delete(
+        prop[GrpcStub](_.methodDescriptionId).in(methodDescriptions.filter(_.scope == Scope.Countdown).map(_.id)) &&
+          prop[GrpcStub](_.times) <= Option(refineMV[NonNegative](0))
+      )
+      _ <- log.info("Purging countdown grpc stubs: {} deleted", deleted6)
     } yield deleted
   }
 
@@ -60,8 +83,10 @@ final class EphemeralCleaner(stubDAO: HttpStubDAO[Task], scenarioDAO: ScenarioDA
 object EphemeralCleaner {
   val live = ZLayer {
     for {
-      hsd <- ZIO.service[HttpStubDAO[Task]]
-      sd  <- ZIO.service[ScenarioDAO[Task]]
-    } yield new EphemeralCleaner(hsd, sd)
+      hsd  <- ZIO.service[HttpStubDAO[Task]]
+      sd   <- ZIO.service[ScenarioDAO[Task]]
+      gsd  <- ZIO.service[GrpcStubDAO[Task]]
+      gmdd <- ZIO.service[GrpcMethodDescriptionDAO[Task]]
+    } yield new EphemeralCleaner(hsd, sd, gsd, gmdd)
   }
 }
