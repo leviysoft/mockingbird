18dec78ab91411e3e651f05e40172d9628f7d5c8
Daniel Slapman <danslapman@gmail.com>
2025-01-02 23:53:54 +0100

    Implement Advxml-based XmlPredicate

---
+++ backend/mockingbird/src/main/scala/ru/tinkoff/tcb/predicatedsl/xml/XmlPredicate2.scala
@@ -0,0 +1,136 @@
+package ru.tinkoff.tcb.predicatedsl.xml
+
+import scala.xml.NodeSeq
+
+import cats.data.NonEmptyList
+import cats.data.Validated
+import cats.data.ValidatedNel
+import io.circe.Decoder
+import io.circe.Encoder
+import io.circe.Json
+import io.circe.parser.parse
+import org.bson.BsonInvalidOperationException
+import sttp.tapir.Schema
+
+import ru.tinkoff.tcb.bson.BsonDecoder
+import ru.tinkoff.tcb.bson.BsonEncoder
+import ru.tinkoff.tcb.circe.bson.*
+import ru.tinkoff.tcb.generic.RootOptionFields
+import ru.tinkoff.tcb.instances.predicate.and.*
+import ru.tinkoff.tcb.predicatedsl.Keyword
+import ru.tinkoff.tcb.predicatedsl.Keyword.*
+import ru.tinkoff.tcb.predicatedsl.PredicateConstructionError
+import ru.tinkoff.tcb.predicatedsl.SpecificationError
+import ru.tinkoff.tcb.predicatedsl.json.JsonPredicate
+import ru.tinkoff.tcb.protocol.json.*
+import ru.tinkoff.tcb.protocol.schema.*
+import ru.tinkoff.tcb.utils.circe.*
+import ru.tinkoff.tcb.utils.circe.optics.JsonOptic
+import ru.tinkoff.tcb.utils.json.JObject
+import ru.tinkoff.tcb.xpath.SXpath
+
+abstract class XmlPredicate2 extends (NodeSeq => Boolean) {
+  def definition: XmlPredicate2.Spec
+
+  override def hashCode(): Int = definition.hashCode()
+
+  override def equals(obj: Any): Boolean = obj match {
+    case xp: XmlPredicate2 => xp.definition == definition
+    case _                 => false
+  }
+}
+
+object XmlPredicate2 {
+  type Spec = Map[SXpath, Map[Keyword.Xml, Json]]
+
+  implicit val xmlPredicateDecoder: Decoder[XmlPredicate2] =
+    Decoder[Spec].emap(apply(_).toEither.leftMap(_.toList.mkString(", ")))
+
+  implicit val xmlPredicateEncoder: Encoder[XmlPredicate2] =
+    Encoder[Spec].contramap(_.definition)
+
+  implicit val xmlPredicateSchema: Schema[XmlPredicate2] =
+    implicitly[Schema[Spec]].as[XmlPredicate2]
+
+  implicit val xmlPredicateBsonDecoder: BsonDecoder[XmlPredicate2] =
+    BsonDecoder[Spec].afterReadTry(
+      apply(_).toEither.leftMap(errs => new BsonInvalidOperationException(errs.toList.mkString(", "))).toTry
+    )
+
+  implicit val xmlPredicateBsonEncoder: BsonEncoder[XmlPredicate2] =
+    BsonEncoder[Spec].beforeWrite(_.definition)
+
+  implicit val xmlPredicateRootOptionFields: RootOptionFields[XmlPredicate2] =
+    RootOptionFields.mk[XmlPredicate2](RootOptionFields[Spec].fields, RootOptionFields[Spec].isOptionItself)
+
+  /**
+   * @param description
+   *   Looks like: {"/xpath": <predicate description>]
+   * @return
+   */
+  def apply(
+      description: Spec
+  ): ValidatedNel[PredicateConstructionError, XmlPredicate2] =
+    description.toVector
+      .map { case (xPath, spec) =>
+        spec.toVector
+          .map(mkPredicate.tupled)
+          .reduceOption(_ |+| _)
+          .getOrElse(Validated.valid((_: Option[NodeSeq]) => true))
+          .leftMap(errors => NonEmptyList.one(SpecificationError(xPath.raw, errors)))
+          .map(pred => (n: NodeSeq) => xPath.toZoom.bind(n).run[Option].pipe(pred))
+      }
+      .reduceOption(_ |+| _)
+      .getOrElse(Validated.valid((_: NodeSeq) => true))
+      .map(f =>
+        new XmlPredicate2 {
+          override val definition: Map[SXpath, Map[Keyword.Xml, Json]] = description
+
+          override def apply(xml: NodeSeq): Boolean = f(xml)
+        }
+      )
+
+  private val mkPredicate: (Keyword.Xml, Json) => ValidatedNel[(Keyword, Json), Option[NodeSeq] => Boolean] =
+    (kwd, jv) =>
+      (kwd, jv) match {
+        case (Equals, JsonString(str)) =>
+          Validated.valid(_.exists(_.text == str))
+        case (Equals, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => jnum.toBigDecimal.contains(BigDecimal(n.text))))
+        case (NotEq, JsonString(str)) =>
+          Validated.valid(_.exists(_.text != str))
+        case (NotEq, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => !jnum.toBigDecimal.contains(BigDecimal(n.text))))
+        case (Greater, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => jnum.toBigDecimal.exists(_ < BigDecimal(n.text))))
+        case (Gte, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => jnum.toBigDecimal.exists(_ <= BigDecimal(n.text))))
+        case (Less, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => jnum.toBigDecimal.exists(_ > BigDecimal(n.text))))
+        case (Lte, JsonNumber(jnum)) =>
+          Validated.valid(_.exists(n => jnum.toBigDecimal.exists(_ >= BigDecimal(n.text))))
+        case (Rx, JsonString(str)) =>
+          Validated.valid(_.exists(_.text.matches(str)))
+        case (Size, JsonNumber(jnum)) if jnum.toInt.isDefined =>
+          Validated.valid(_.exists(_.text.length == jnum.toInt.get))
+        case (Exists, JsonBoolean(true))  => Validated.valid(_.isDefined)
+        case (Exists, JsonBoolean(false)) => Validated.valid(_.isEmpty)
+        case (Cdata, JsonDocument(JObject("==" -> JsonString(value)))) =>
+          Validated.valid(_.exists(_.text == value))
+        case (Cdata, JsonDocument(JObject("~=" -> JsonString(value)))) =>
+          Validated.valid(_.exists(_.text.matches(value)))
+        case (JCdata, spec) =>
+          Validated
+            .fromEither(for {
+              jspec <- spec
+                .as[Map[JsonOptic, Map[Keyword.Json, Json]]]
+                .leftMap(_ => (kwd.asInstanceOf[Keyword] -> spec))
+              jpred <- JsonPredicate(jspec).toEither.leftMap(errs =>
+                (kwd.asInstanceOf[Keyword] -> Json.fromString(errs.toList.mkString(", ")))
+              )
+            } yield jpred)
+            .leftMap(NonEmptyList.one)
+            .map(jpred => (op: Option[NodeSeq]) => op.map(_.text).flatMap(parse(_).toOption).map(jpred).getOrElse(false))
+        case (kwd, j) => Validated.invalidNel(kwd -> j)
+      }
+}
---
+++ backend/mockingbird/src/test/scala/ru/tinkoff/tcb/predicatedsl/xml/XmlPredicateSpec2.scala
@@ -0,0 +1,236 @@
+package ru.tinkoff.tcb.predicatedsl.xml
+
+import cats.data.NonEmptyList
+import cats.scalatest.EitherValues
+import io.circe.Json
+import io.circe.syntax.*
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import ru.tinkoff.tcb.predicatedsl.Keyword
+import ru.tinkoff.tcb.predicatedsl.SpecificationError
+import ru.tinkoff.tcb.xpath.*
+
+class XmlPredicateSpec2 extends AnyFunSuite with Matchers with EitherValues {
+  test("XmlPredicate should produce validator from correct specification") {
+    val spec = Json.obj(
+      "/tag1" := Json.obj("==" := "test")
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toEither
+
+    sut shouldBe Symbol("right")
+  }
+
+  test("XmlPredicate should emit correct error for poor specification") {
+    val spec = Json.obj(
+      "/tag1" := Json.obj(">=" := "test")
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toEither
+
+    sut shouldBe Left(
+      NonEmptyList.one(
+        SpecificationError("/tag1", NonEmptyList.one(Keyword.Gte.asInstanceOf[Keyword] -> Json.fromString("test")))
+      )
+    )
+  }
+
+  test("Check equality") {
+    val spec = Json.obj(
+      "/root/tag1" := Json.obj("==" := "test"),
+      // "/root/tag2" := Json.obj("==" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><root><tag1>test</tag1><tag2>42</tag2></root></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><root><tag1>peka</tag1><tag2>42</tag2></root></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check inequality") {
+    val spec = Json.obj(
+      "/root/tag1" := Json.obj("!=" := "test"),
+      "/root/tag2" := Json.obj("!=" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><root><tag1>test</tag1><tag2>42</tag2></root></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><root><tag1>peka</tag1><tag2>99</tag2></root></wrapper>)) shouldBe Some(true)
+  }
+
+  test("Check >") {
+    val spec = Json.obj(
+      "/f" := Json.obj(">" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>43</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>42</f></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check >=") {
+    val spec = Json.obj(
+      "/f" := Json.obj(">=" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>43</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>42</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>41</f></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check <") {
+    val spec = Json.obj(
+      "/f" := Json.obj("<" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>42</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>41</f></wrapper>)) shouldBe Some(true)
+  }
+
+  test("Check <=") {
+    val spec = Json.obj(
+      "/f" := Json.obj("<=" := 42)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>43</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>42</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>41</f></wrapper>)) shouldBe Some(true)
+  }
+
+  test("Check range") {
+    val spec = Json.obj(
+      "/f" := Json.obj(">" := 40, "<=" := 45, "!=" := 43)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>39</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>40</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>41</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>42</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>43</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>44</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>45</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>46</f></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check regex match") {
+    val spec = Json.obj(
+      "f" := Json.obj("~=" := "\\d{4,}")
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>123</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>1234</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>1234a</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>12345</f></wrapper>)) shouldBe Some(true)
+  }
+
+  test("Check size") {
+    val spec = Json.obj(
+      "/f" := Json.obj("size" := 4)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>123</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f>1234</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f>1234a</f></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check exists") {
+    val spec = Json.obj(
+      "/f" := Json.obj("exists" := true)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>123</f></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><f/></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><g>123</g></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check not exists") {
+    val spec = Json.obj(
+      "/f" := Json.obj("exists" := false)
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><f>123</f></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><f/></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><g>123</g></wrapper>)) shouldBe Some(true)
+  }
+
+  test("CDATA equals") {
+    val spec = Json.obj(
+      "/data" := Json.obj(
+        "cdata" := Json.obj(
+          "==" := "test"
+        )
+      )
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><data><![CDATA[test]]></data></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><data><![CDATA[test]]> </data></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><data><![CDATA[kek]]></data></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check CDATA with regex") {
+    val spec = Json.obj(
+      "/data" := Json.obj(
+        "cdata" := Json.obj(
+          "~=" := "\\d+"
+        )
+      )
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper><data><![CDATA[1234]]></data></wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper><data><![CDATA[123f]]></data></wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper><data><![CDATA[1234]]> </data></wrapper>)) shouldBe Some(false)
+  }
+
+  test("Check CDATA with JSON") {
+    val spec = Json.obj(
+      "/json" := Json.obj(
+        "jcdata" := Json.obj(
+          "f" := Json.obj(
+            "==" := 42
+          )
+        )
+      )
+    )
+
+    val sut = XmlPredicate2(spec.as[Map[SXpath, Map[Keyword.Xml, Json]]].value).toOption
+
+    sut.map(_(<wrapper>
+      <json>
+        <![CDATA[{"f": 42}]]>
+      </json>
+    </wrapper>)) shouldBe Some(true)
+    sut.map(_(<wrapper>
+      <json>
+        <![CDATA[{"f": 43}]]>
+      </json>
+    </wrapper>)) shouldBe Some(false)
+    sut.map(_(<wrapper>
+      <json>
+        <![CDATA[{"f": 42]]>
+      </json>
+    </wrapper>)) shouldBe Some(false)
+  }
+}
