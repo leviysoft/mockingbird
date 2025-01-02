6cff94da2cab36a7f06595a36bbd69b92fe5a2e0
Daniel Slapman <danslapman@gmail.com>
2025-01-02 21:48:15 +0100

    Introduce Substitute[Node, Node] instance

--- backend/mockingbird/src/main/scala/ru/tinkoff/tcb/mockingbird/misc/Substitute.scala
+++ backend/mockingbird/src/main/scala/ru/tinkoff/tcb/mockingbird/misc/Substitute.scala
@@ -20,10 +20,9 @@ import ru.tinkoff.tcb.utils.transformation.xml.*
 object Substitute {
   implicit def jsonSJson(implicit sandbox: GraalJsSandbox): Substitute[Json, Json] = (a: Json, b: Json) =>
     a.substitute(b).useAsIs
-  implicit def jsonSNode(implicit sandbox: GraalJsSandbox): Substitute[Json, KNode] = (a: Json, b: KNode) =>
-    a.substitute(b)
+  implicit val jsonSNode: Substitute[Json, KNode] = (a: Json, b: KNode) => a.substitute(b)
   implicit def nodeSJson(implicit sandbox: GraalJsSandbox): Substitute[Node, Json] = (a: Node, b: Json) =>
     a.substitute(b).useAsIs
-  implicit def nodeSnode(implicit sandbox: GraalJsSandbox): Substitute[Node, KNode] = (a: Node, b: KNode) =>
-    a.substitute(b)
+  implicit val nodeSNode: Substitute[Node, Node]   = (a: Node, b: Node) => a.substitute(b)
+  implicit val nodeSKnode: Substitute[Node, KNode] = (a: Node, b: KNode) => a.substitute(b)
 }
