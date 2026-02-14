37f203394a308cc2bde231ef8a908fab1431489d
Daniel Slapman <danslapman@gmail.com>
2026-02-14 18:11:30 +0100

    Update testcontainers

--- backend/project/Dependencies.scala
+++ backend/project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   lazy val testContainers = Seq(
     "testcontainers-scala-scalatest",
     "testcontainers-scala-mongodb",
-  ).map("com.dimafeng" %% _ % "0.41.5" % Test)
+  ).map("com.dimafeng" %% _ % "0.44.1" % Test)
 
   lazy val refined = Seq(
     "eu.timepit" %% "refined" % "0.11.3"
