07bf3e83a6e816657176e509330af95e7c6d5302
Daniel Slapman <danslapman@gmail.com>
2026-02-14 18:23:08 +0100

    Fix attempt #2

--- backend/project/Settings.scala
+++ backend/project/Settings.scala
@@ -41,7 +41,7 @@ object Settings {
       "-feature",
       "-language:higherKinds",
       "-language:implicitConversions",
-      "-release:21",
+      "-release", "21",
       "-unchecked",
       "-Xmax-inlines", "64",
       "-Ybackend-parallelism",
